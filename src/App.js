5ab269d47c06d2ac7c503a7aacd0b3948c25ec75
yannick <yannickmacken@gmail.com>
2023-10-15 10:10:00 +0200

    frontend with placeholder endpoints

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(''); // To store the current input number
+  const [numbersList, setNumbersList] = useState([]); // To store the list of numbers
+
+  // Function to handle submitting the number to backend
+  const handleSubmitNumber = async () => {
+    try {
+      // Assuming you have a backend endpoint '/submitNumber' to POST the number
+      await fetch('/submitNumber', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ number }),
+      });
+
+      setNumber(''); // Clear the input after submitting
+    } catch (err) {
+      console.error('Error submitting number:', err);
+    }
+  };
+
+  // Function to fetch all submitted numbers from backend
+  const handleFetchNumbers = async () => {
+    try {
+      const response = await fetch('/getNumbers');
+      const data = await response.json();
+      setNumbersList(data.numbers || []); // Assuming the backend returns an object with a 'numbers' property
+    } catch (err) {
+      console.error('Error fetching numbers:', err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
-      </header>
+      <div className="App-header">
+        <h2>Submit population count</h2>
+        <div>
+          <input
+            type="number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+            placeholder="Enter a number"
+          />
+          <button onClick={handleSubmitNumber}>Submit count</button>
+        </div>
+        <button onClick={handleFetchNumbers}>Get population count over time</button>
+        <ul>
+          {numbersList.map((num, idx) => (
+            <li key={idx}>{num}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
